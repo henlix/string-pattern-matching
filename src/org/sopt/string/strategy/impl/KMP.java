6f1fa8c7e198b2c21ea35a35d4b5b4adab28644a
donghyundonghyun <andy5153a@naver.com>
2015-12-31 15:01:37 +0900

    add KMP algorithm code

--- src/org/sopt/string/strategy/impl/KMP.java
+++ src/org/sopt/string/strategy/impl/KMP.java
@@ -1,5 +1,6 @@
 package org.sopt.string.strategy.impl;
 
+import org.sopt.string.domain.Result;
 import org.sopt.string.domain.ResultSet;
 import org.sopt.string.strategy.base.Strategy;
 
@@ -7,7 +8,47 @@ public class KMP implements Strategy {
 
     @Override
     public ResultSet find(String from, String target) {
+        ResultSet set = new ResultSet();
 
-        throw new UnsupportedOperationException("Must implement algorithm !!");
+        int[] inform = preprocessing(target);
+
+        int i = 0;
+        int j = 0;
+
+        while(i < from.length()){
+            if(j == -1 || from.charAt(i) == target.charAt(j)){
+                i++; j++;
+            }
+            else {
+                j = inform[j];
+            }
+            if( j == target.length() ) {
+                set.add(new Result(i - target.length(),i-1));
+                j = inform[j];
+            }
+        }
+
+        return set;
+    }
+
+    private int[] preprocessing(String target) {
+        int[] inform = new int[target.length()+1];
+
+        int j = 1;
+        int k = 0;
+
+        inform[0] = -1;
+
+
+        while(j < target.length()) {
+            if(k == -1 || target.charAt(j) == target.charAt(k)){
+                j++; k++;
+                inform[j] = k;
+            }
+            else {
+                k = inform[k];
+            }
+        }
+        return inform;
     }
 }
